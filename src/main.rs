cd8d0c2ee2082c5f9ce5cc0c775d6c7b5fbd8901
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-01-19 19:41:13 +0500

    whoops missed crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,61 @@
+use std::error::Error;
+use std::{io, thread, time::Duration};
 use std::process::Command;
-use std::thread::{self,sleep};
+use thread::sleep;
 use signal_hook::{consts::SIGINT, iterator::Signals};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal,
+};
 
-fn main() {
-    
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+fn main() -> Result<(),std::io::Error>{
     handl_ctrlc();
-
-    new_buffer();
-    clear_screen();
-    println!("Hello, world!");
-    let mut buffer = String::new();
-    let x = std::io::stdin().read_line(&mut buffer).unwrap_or(0);
-
-    exit_buffer(true);
-    dbg!(x);
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+    
+    thread::sleep(Duration::from_millis(5000));
+    
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    
+    Ok(())
+    
+    
+    //
+    // new_buffer();
+    // clear_screen();
+    // println!("Hello, world!");
+    // let mut buffer = String::new();
+    // let x = std::io::stdin().read_line(&mut buffer).unwrap_or(0);
+    //
+    // exit_buffer(true);
+    // dbg!(x);
 }
 
 
@@ -46,4 +88,5 @@ fn handl_ctrlc(){
 // fn notify_send(message:&str){
 //     Command::new("notify-send").args(["--urgency=critical", message]).output().expect("failed");
 // }
+
 
